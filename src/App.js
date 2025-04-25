f370cbb35da6dbaec223af70185b2d50c395cdbd
Emanuel Junior <emanuel.c.junior@hotmail.com>
2025-04-25 10:20:40 -0300

    Projeto inicial organo

--- src/App.js
+++ src/App.js
@@ -1,23 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
+import Banner from './componentes/Banner/index.js';
+import Formulario from './componentes/Formulario/index.js';
+import Time from './componentes/Time/index.js';
+import Rodape from './componentes/Rodape/index.js';
 
 function App() {
+
+  const times = [
+    {
+      nome: 'Programação',
+      corPrimaria: '#57C278',
+      corSegundaria: '#D9F7E9',
+    },
+    {
+      nome: 'Front-End',
+      corPrimaria: '#82CFFA',
+      corSegundaria: '#E8F8FF',
+    },
+    ,
+    {
+      nome: 'Data Science',
+      corPrimaria: '#A6D157',
+      corSegundaria: '#F0F8E2',
+    },
+    {
+      nome: 'Devops',
+      corPrimaria: '#A6D157',
+      corSegundaria: '#F0F8E2',
+    }
+  ]
+  const [colaboradores, setColaboradores] = useState([])
+
+  const aoColaboradorAdicionado = (colaborador) => {
+    debugger
+    console.log("Colaborador add =>", colaborador)
+    setColaboradores([...colaboradores, colaborador])
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Banner />
+      <Formulario times={times.map(time => time.nome)} aoColaboradorCadastrado={colaborador => aoColaboradorAdicionado(colaborador)} />
+
+      {times.map(time => <Time
+        key={time.nome}
+        nome={time.nome}
+        corPrimaria={time.corPrimaria}
+        corSegundaria={time.corSegundaria}
+        colaboradores={colaboradores.filter(colaborador => colaborador.time === time.nome)} />)}
+      <Rodape />
     </div>
   );
 }
---
+++ src/componentes/Banner/index.js
@@ -0,0 +1,14 @@
+import { Fragment } from 'react'
+import './Banner.css'
+
+const Banner = () => {
+    return (
+        <Fragment>
+            <header className="banner">
+                <img src="./imagens/banner.png"></img>
+            </header>
+        </Fragment>
+    )
+}
+
+export default Banner
